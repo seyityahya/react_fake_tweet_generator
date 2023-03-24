05e5735b02bec61ccc54c9592cf82712c8c31876
seyityahya <seyityahya05@gmail.com>
2023-03-25 02:43:33 +0300

    ui started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
-
 function App() {
-  return <div>deneme</div>;
+  return (
+    <div className="tweet">
+      <div className="tweet-author">
+        <img
+          alt="seyityahya"
+          src="https://pbs.twimg.com/profile_images/1586885263204257800/GbgLBTGx_bigger.jpg"
+        />
+        <div>
+          <div className="name">Seyit Yahya</div>
+          <div className="username">seyityahya3</div>
+        </div>
+      </div>
+      <div className="tweet-content">
+        <p>
+          Bu bir tweetter uygulamasıdır ve buradan fake tweetler
+          üretilebilecektir.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
